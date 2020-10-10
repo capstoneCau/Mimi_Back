5330dc055dfdcbe3ef216294733d45f188f1af46
whddk4415 <whddk4415@naver.com>
2020-10-10 22:43:11 +0900

    python에는 존재하지 않는 null값을 None값으로 교체

--- mimi_server/apps/mail/views.py
+++ mimi_server/apps/mail/views.py
@@ -11,16 +11,16 @@ import random
 @api_view(['POST'])
 def mail(request):
     sub = forms.Mail()
+    print(request.data)
     if request.method == 'POST':
-        if request.data["address"] != null :
+        if request.data['address'] != None :
             ranNum = random.randint(100000, 999999)
             sub = forms.Mail(request.POST)
             subject = 'Welcome to Mimi'
             message = f"You input {ranNum}"
-            recepient = request.data["address"]
-            send_mail(subject, 
-                message, EMAIL_HOST_USER, [recepient], fail_silently = False)
+            recepient = request.data['address']
+            send_mail(subject, message, EMAIL_HOST_USER, [recepient], fail_silently = False)
             return Response({"token": ranNum}, status=status.HTTP_201_CREATED)
-        elif request.data["address"] == null :
+        elif request.data['address'] == None :
             return Response({"error": "Not be inputted Address"}, status=status.HTTP_400_BAD_REQUEST)
     return render(request, 'mail/index.html', {'form':sub})
