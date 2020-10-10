a9bc445220b4aab19490b1db7187a6ed7fd2a4b4
whddk4415 <whddk4415@naver.com>
2020-10-10 15:05:51 +0900

    POST 요청시 메일 송신 후 Auth token 을 보냄

--- mimi_server/apps/mail/views.py
+++ mimi_server/apps/mail/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render
 from mimi_server.settings import EMAIL_HOST_USER
 from . import forms
 from django.core.mail import send_mail
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+import random
 # Create your views here.
 #DataFlair #Send Email
+@api_view(['POST'])
 def mail(request):
     sub = forms.Mail()
     if request.method == 'POST':
-        sub = forms.Mail(request.POST)
-        subject = 'Welcome to DataFlair'
-        message = 'Hope you are enjoying your Django Tutorials'
-        recepient = str(sub['Email'].value())
-        send_mail(subject, 
-            message, EMAIL_HOST_USER, [recepient], fail_silently = False)
-        return render(request, 'mail/success.html', {'recepient': recepient})
+        if request.data["address"] != null :
+            ranNum = random.randint(100000, 999999)
+            sub = forms.Mail(request.POST)
+            subject = 'Welcome to Mimi'
+            message = f"You input {ranNum}"
+            recepient = request.data["address"]
+            send_mail(subject, 
+                message, EMAIL_HOST_USER, [recepient], fail_silently = False)
+            return Response({"token": ranNum}, status=status.HTTP_201_CREATED)
+        elif request.data["address"] == null :
+            return Response({"error": "Not be inputted Address"}, status=status.HTTP_400_BAD_REQUEST)
     return render(request, 'mail/index.html', {'form':sub})
