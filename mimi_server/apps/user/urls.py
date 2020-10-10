15bf85b213a57cc91125f91c065f301eb4e24049
whddk4415 <whddk4415@naver.com>
2020-10-10 22:47:46 +0900

    별자리, 학교, MBTI 목록을 가져오기 위해 url 및 view 추가.

--- mimi_server/apps/user/urls.py
+++ mimi_server/apps/user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet
+from .views import UserViewSet, SchoolViewSet, MbtiViewSet, StarViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'users', UserViewSet)
+router.register(r'school', SchoolViewSet)
+router.register(r'mbti', MbtiViewSet)
+router.register(r'star', StarViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- mimi_server/apps/user/views.py
+++ mimi_server/apps/user/views.py
@@ -1,10 +1,25 @@
-from .models import User
+from .models import User, School, Mbti, Star
 from rest_framework import viewsets
 from rest_framework import mixins
-from .serializer import UserSerializer
+from .serializer import UserSerializer, SchoolSerializer, MbtiSerializer, StarSerializer
 
 
-class UserViewSet(mixins.UpdateModelMixin, viewsets.ReadOnlyModelViewSet):
+class UserViewSet(mixins.UpdateModelMixin, mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'kakao_auth_id'
+
+class SchoolViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
+    lookup_field = 'name'
+
+class MbtiViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Mbti.objects.all()
+    serializer_class = MbtiSerializer
+    lookup_field = 'name'
+
+class StarViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Star.objects.all()
+    serializer_class = StarSerializer
+    lookup_field = 'name'
