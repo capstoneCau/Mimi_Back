4b111df2063e14345807a6c3696c827557c2fa45
whddk4415 <whddk4415@naver.com>
2020-10-13 02:00:33 +0900

    meeting app 등록

--- mimi_server/settings.py
+++ mimi_server/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mimi_server.apps.user',
-    # 'mimi_server.apps.meeting',
+    'mimi_server.apps.meeting',
     'mimi_server.apps.mail',
     'rest_framework',
     # CORS
@@ -123,13 +123,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
