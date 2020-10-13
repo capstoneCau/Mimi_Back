2b8fac7c82761589c98d472d83f116dec290974b
whddk4415 <whddk4415@naver.com>
2020-10-13 19:18:02 +0900

    User view에 등록 되어 있던 School, Animal, Star, Mbti View를 etcInformation으로 이동, Zodiac View 생성 및 각 궁합도 View 생성

--- mimi_server/apps/user/views.py
+++ mimi_server/apps/user/views.py
@@ -1,25 +1,11 @@
-from .models import User, School, Mbti, Star
+from .models import User
 from rest_framework import viewsets
 from rest_framework import mixins
-from .serializer import UserSerializer, SchoolSerializer, MbtiSerializer, StarSerializer
-
+from .serializer import UserSerializer
 
 class UserViewSet(mixins.UpdateModelMixin, mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'kakao_auth_id'
 
-class SchoolViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = School.objects.all()
-    serializer_class = SchoolSerializer
-    lookup_field = 'name'
 
-class MbtiViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Mbti.objects.all()
-    serializer_class = MbtiSerializer
-    lookup_field = 'name'
-
-class StarViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Star.objects.all()
-    serializer_class = StarSerializer
-    lookup_field = 'name'
