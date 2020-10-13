327a3d2b8c7bffd1019461b8f1a5f908259b267e
whddk4415 <whddk4415@naver.com>
2020-10-13 19:17:24 +0900

    User에 있던 School, Animal, Mbti, Star Serializer를 etcInformation으로 이동, Zodiac Serializer 생성, 각 궁합도 Serializer 생성

--- mimi_server/apps/user/serializer.py
+++ mimi_server/apps/user/serializer.py
@@ -1,34 +1,13 @@
 from rest_framework import serializers
-from .models import User, Animal, School, Mbti, Friends, Star
+from .models import User
 from collections import OrderedDict
-class AnimalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Animal
-        fields = ['label']
-    
-class SchoolSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = School
-        fields = ['name', 'email_info']
-    
-class MbtiSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mbti
-        fields = ['name', 'description']
-
-class StarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Star
-        fields = ['name', 'start_date', 'end_date']
-    
-
 
 class UserSerializer(serializers.ModelSerializer):
     # profileImg = AnimalSerializer()
     class Meta:
         model = User
         fields = ['kakao_auth_id', 'name', 'gender', 'birthday', 'address','email', 'school',
-        'profileImg', 'mbti', 'star', 'kakao_id']
+        'profileImg', 'mbti', 'star', 'chinese_zodiac', 'kakao_id']
         # fields = '__all__'
         lookup_field = 'kakao_auth_id'
         extra_kwargs = {
