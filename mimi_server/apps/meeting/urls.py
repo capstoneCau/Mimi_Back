0ac6c3a39ea3c62805ff206dc9734dd6900c3460
whddk4415 <whddk4415@naver.com>
2020-10-13 12:50:52 +0900

    Viewset 이름 재 정의

--- mimi_server/apps/meeting/urls.py
+++ mimi_server/apps/meeting/urls.py
@@ -3,7 +3,8 @@ from rest_framework import routers
 
 from mimi_server.apps.meeting.models import Room, Meeting
 from mimi_server.apps.user.models import FriendsParticipation
-from mimi_server.apps.meeting.views import AllRoomViewSet, MeetingCreateRequestViewSet, MyRoomViewSet, MeetingCreateRequestViewSet
+from mimi_server.apps.meeting.views import AllRoomViewSet, MeetingCreateRequestViewSet, \
+    MyRoomViewSet, MeetingCreateRequestViewSet
 
 router = routers.DefaultRouter()
 router.register(r'allRoomList', AllRoomViewSet, basename='allRoom')
