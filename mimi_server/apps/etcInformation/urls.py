96cf0ca235387d0b44e45a0eec2f3ac32a6ccdbb
whddk4415 <whddk4415@naver.com>
2020-10-13 19:16:42 +0900

    User url에 등록되어 있던 Animal, School, Star, Mbti 를 etcInformaion으로 이동 및 zodiac url, 각 궁합도 url 등록

---
+++ mimi_server/apps/etcInformation/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import AnimalViewSet, SchoolViewSet, MbtiViewSet, StarViewSet, ChineseZodiacViewSet, \
+    StarCompatibilityViewSet, MbtiCompatibilityViewSet, ZodiacCompatibilityViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'animal', AnimalViewSet)
+router.register(r'school', SchoolViewSet)
+router.register(r'mbti', MbtiViewSet)
+router.register(r'star', StarViewSet)
+router.register(r'zodiac', ChineseZodiacViewSet)
+router.register(r'compatibility/mbti', MbtiCompatibilityViewSet, 'mbtiCompatibility')
+router.register(r'compatibility/star', StarCompatibilityViewSet, 'starCompatibility')
+router.register(r'compatibility/zodiac', ZodiacCompatibilityViewSet, 'zodiacCompatibility')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
