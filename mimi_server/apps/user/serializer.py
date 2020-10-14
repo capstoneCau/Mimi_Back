d68c9073f3d2756fc19a79749bdf6b585aafe047
whddk4415 <whddk4415@naver.com>
2020-10-14 14:41:47 +0900

    기존 AuthTokeSerializer를 커스텀하여 프로젝트에 맞도록 변경

--- mimi_server/apps/user/serializer.py
+++ mimi_server/apps/user/serializer.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from .models import User
 from collections import OrderedDict
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
 
 class UserSerializer(serializers.ModelSerializer):
     # profileImg = AnimalSerializer()
     class Meta:
         model = User
-        fields = ['kakao_auth_id', 'name', 'gender', 'birthday', 'address','email', 'school',
-        'profileImg', 'mbti', 'star', 'chinese_zodiac', 'kakao_id']
+        fields = ['kakao_auth_id', 'name', 'gender', 'birthday','email', 'school',
+        'profileImg', 'latitude', 'longitude', 'mbti', 'star', 'chinese_zodiac']
         # fields = '__all__'
         lookup_field = 'kakao_auth_id'
         extra_kwargs = {
@@ -37,3 +39,30 @@ class UserField(serializers.PrimaryKeyRelatedField):
             return {}
 
         return OrderedDict([(item.kakao_auth_id, self.display_value(item)) for item in queryset])
+
+
+
+
+class CustomAuthTokenSerializer(serializers.Serializer):
+    kakao_auth_id = serializers.CharField(label=_("Kakao_auth_id"))
+
+    def validate(self, attrs):
+        kakao_auth_id = attrs.get('kakao_auth_id')
+        password = "mimi"
+
+        if kakao_auth_id and password:
+            user = authenticate(request=self.context.get('request'),
+                                kakao_auth_id=kakao_auth_id, password=password)
+
+            # The authenticate call simply returns None for is_active=False
+            # users. (Assuming the default ModelBackend authentication
+            # backend.)
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "username" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
