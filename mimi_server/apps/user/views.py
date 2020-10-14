4adbc589e8730510333e77a5af3d97aad8f064e1
whddk4415 <whddk4415@naver.com>
2020-10-14 14:46:00 +0900

    기존 UserView를 Token을 발급 받을 수 있도록 변경, AuthToken을 서버에 맞게 Custom하여 제작

--- mimi_server/apps/user/views.py
+++ mimi_server/apps/user/views.py
@@ -1,11 +1,75 @@
-from .models import User
-from rest_framework import viewsets
-from rest_framework import mixins
-from .serializer import UserSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.views import APIView
+from mimi_server.apps.user.serializer import UserSerializer, CustomAuthTokenSerializer
 
-class UserViewSet(mixins.UpdateModelMixin, mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
-    queryset = User.objects.all()
+from rest_framework import parsers, renderers
+# from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.compat import coreapi, coreschema
+from rest_framework.schemas import ManualSchema
+
+class CreateUserAPIView(CreateAPIView):
     serializer_class = UserSerializer
-    lookup_field = 'kakao_auth_id'
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        # We create a token than will be used for future auth
+        token = Token.objects.create(user=serializer.instance)
+        token_data = {"token": token.key}
+        return Response(
+            {**serializer.data, **token_data},
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
 
 
+class LogoutUserAPIView(APIView):
+    queryset = get_user_model().objects.all()
+
+    def get(self, request, format=None):
+        # simply delete the token to force a login
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
+
+class CustomAuthToken(APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = CustomAuthTokenSerializer
+    if coreapi is not None and coreschema is not None:
+        schema = ManualSchema(
+            fields=[
+                coreapi.Field(
+                    name="kakao_auth_id",
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Kakao_auth_id",
+                        description="Valid Kakao_auth_id for authentication",
+                    ),
+                )
+            ],
+            encoding="application/json",
+        )
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+obtain_auth_token = CustomAuthToken.as_view()
