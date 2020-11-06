32a686169356bb0fc495a8f95807cf5a3b612523
whddk4415 <whddk4415@naver.com>
2020-11-07 01:54:15 +0900

    유저를 찾을 때 유저의 카카오 ID가 아닌 학교 이메일을 사용하여 찿도록 추가

--- mimi_server/apps/user/user_urls.py
+++ mimi_server/apps/user/user_urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import FriendsViewSet
+from .views import FriendsViewSet, SearchUserView
 
 router = routers.DefaultRouter()
 
 router.register(r'friends', FriendsViewSet, basename="friends")
-
+router.register('search', SearchUserView, basename="search")
 urlpatterns = [
     path('', include(router.urls)),
 ]
