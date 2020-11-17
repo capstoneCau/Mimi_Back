6d2ec61958ae95957101dffe2d165842a9e3f58c
whddk4415 <whddk4415@naver.com>
2020-11-17 17:53:07 +0900

    얼굴 인식을 kakao vision api를 사용하도록 수정하였음. 따라서 필요없어진 Cascade 파일 삭제

--- mimi_server/apps/image/animal_similarity.py
+++ mimi_server/apps/image/animal_similarity.py
@@ -8,33 +8,27 @@ from imageio import imread
 # 2. Model 불러오기
 from tensorflow.python.keras.models import load_model
 from mimi_server.settings import BASE_DIR
+from .kakaoFaceAPI import faceDetect
 
 def determinAnimal(imageData, gender) :
-    cascadePath = os.path.join(BASE_DIR, 'CascadeClassifier')
     modelPath = os.path.join(BASE_DIR, 'model')
     if gender == 'male' :
         model = load_model(os.path.join(modelPath, 'animal_model_man.h5'), compile = False)
     elif gender == 'female' :
         model = load_model(os.path.join(modelPath, 'animal_model_woman.h5'), compile = False)
     categories = ['dog','cat','bear','hamster','horse', 'wolf']
-    
-    test_img = imread(io.BytesIO(base64.b64decode(imageData)))
-    face_cascade = cv2.CascadeClassifier(os.path.join(cascadePath, 'haarcascade_frontalface_default.xml'))
-    eye_casecade = cv2.CascadeClassifier(os.path.join(cascadePath, 'haarcascade_eye.xml'))
+    image = base64.b64decode(imageData)
+    try :
+        x, y, w, h = faceDetect(image)
+    except ValueError:
+        return {"detail" : "Please take a picture alone.", "error" : 409}
 
-    test = np.empty(1, dtype=np.float32)
-    try:
-        gray = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1,3)
-    except Exception as e:
-        return {"detail" : "Image file is not found", "error" : 400}
-    for (x,y,w,h) in faces:
-        try:
-            cropped = test_img[y - int(h / 4):y + h + int(h / 4), x - int(w / 4):x + w + int(w / 4)]
-            if cropped.shape > test.shape:
-                test = cropped
-        except Exception as e:
-            return {"detail" : "The face is not recognized in the picture.", "error" : 404}
+    test_img = imread(io.BytesIO(image))
+    
+    height, width = test_img.shape[0], test_img.shape[1]
+    cropped = test_img[max(0, y - int(h / 4)): min(height, y + h + int(h / 4)), max(0, x - int(w / 4)):min(width, x + w + int(w / 4))]
+
+    test = cropped
     test = test.astype('float32')/255.            
     test = cv2.resize(test,(128,128))
     test = np.expand_dims(test,0)
@@ -44,7 +38,7 @@ def determinAnimal(imageData, gender) :
         return {"detail" : "The face is not recognized in the picture.", "error" : 404}
     sortedIndex = np.argsort(y_predicted)[0][::-1]
     np.set_printoptions(suppress=True)
-    # print(y_predicted[0], sortedIndex)
+    print(y_predicted[0], sortedIndex)
     retunValue = [
         {
             'category' : categories[sortedIndex[0]],
@@ -59,5 +53,5 @@ def determinAnimal(imageData, gender) :
             'predict_rate' : y_predicted[0][sortedIndex[2]]
         }
     ]
-    # print(retunValue)
+    print(retunValue)
     return retunValue
